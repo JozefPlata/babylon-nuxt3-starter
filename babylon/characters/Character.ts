d50d46b40d90fd365a6179c76848a1e37c654e46
jozef <plata.jozef@protonmail.com>
2022-09-24 07:56:44 +0300

    Base Character class

--- babylon/characters/Character.ts
+++ babylon/characters/Character.ts
@@ -1,15 +1,27 @@
-import {Mesh, Scene, TransformNode, Vector3} from "@babylonjs/core";
+import {Camera, Mesh, Scene, TransformNode, Vector3} from "@babylonjs/core";
 import {Constraint} from "~/babylon/player/Constraint";
 
 export class Character extends TransformNode {
-    private readonly _camera;
+    private _type: CharacterType;
     private readonly _model: Mesh;
     private readonly _rotator: TransformNode;
     private readonly _rotationConstraint: Constraint;
 
-    constructor(name: string, model: Mesh, position: Vector3, scene: Scene) {
+    constructor(type: CharacterType, name: string, model: Mesh, position: Vector3, constraintTarget: Camera | TransformNode, scene: Scene) {
         super(name, scene);
+        this._type = type;
         this._model = model;
         this.position = position;
+        this._rotator = new TransformNode('rotator', scene);
+        this._rotationConstraint = new Constraint(this._rotator, constraintTarget);
+
+        this._model.setParent(this);
+        this._rotator.setParent(this);
+        constraintTarget.parent = this;
     }
+}
+
+export enum CharacterType {
+    PLAYER,
+    NPC
 }
--- babylon/player/Constraint.ts
+++ babylon/player/Constraint.ts
@@ -2,15 +2,18 @@ import {Camera, TransformNode} from "@babylonjs/core";
 
 export class Constraint {
     private _object: TransformNode;
-    private _to: Camera;
+    private readonly _to: Camera | TransformNode;
 
-    constructor(object: TransformNode, to: Camera) {
+    constructor(object: TransformNode, to: Camera | TransformNode) {
         this._to = to;
         this._object = object;
     };
 
     public active() {
-        this._object.rotation = this._to.absoluteRotation.toEulerAngles();
+        if (this._to instanceof Camera)
+            this._object.rotation = this._to.absoluteRotation.toEulerAngles();
+        else if (this._to instanceof TransformNode)
+            this._object.rotationQuaternion = this._to.rotationQuaternion;
         this._object.rotation.x = 0;
         this._object.rotation.z = 0;
     }
